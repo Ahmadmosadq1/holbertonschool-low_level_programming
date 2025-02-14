763f3471f53358c5d90c39dd70930de7dfc71541
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-02-14 05:26:18 +0000

    5-printf.c

---
+++ hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: This function prints a string using printd.
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
+
