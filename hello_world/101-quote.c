736ae1eff1b1d29175318e8d962aef904ac7a640
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-02-14 21:02:10 +0000

    101-quote.c

---
+++ hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: This function prints a string using printf.
+ * Return: Always 0 (Success)
+ */
+
+int main()
+
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	
+	return (1);
+
+}
