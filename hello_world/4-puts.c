f2447e904cabd9519c0d2ccfe2900ff4adc838cd
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-02-14 01:22:50 +0000

    4-puts.c

---
+++ hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*
+ * main- the entry point of the function
+ *
+ * Desciption:This scipt is about running a string using puts function
+ *
+ * Return: ends with 0 (sucess)
+ */
+
+int main(void)
+
+{
+	/*printing using puts function*/
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
+
