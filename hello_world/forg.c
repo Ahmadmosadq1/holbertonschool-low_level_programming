ee30e788b6e28c62da01360f1f56b2df0eba5afd
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-02-14 07:02:18 +0000

    6-size.c

---
+++ hello_world/forg.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: The script prints the sizes of the different data type.
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+printf("%s\n","hello");
+return (0);
+}
