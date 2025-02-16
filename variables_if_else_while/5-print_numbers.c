24bde99c3d10cded33da207b9a54b8072d89a638
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-02-16 02:35:11 +0000

    5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: printing numbers from 0 to 9.
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+	{
+	int num = 0;
+
+	while (num <= 9)
+	{
+	printf("%d", num);
+	num++;
+	}
+	printf("\n");
+	return (0);
+	}
