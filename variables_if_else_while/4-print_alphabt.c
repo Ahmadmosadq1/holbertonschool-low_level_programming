713d32b7b96615ecf227e20ff30fd615e43e4403
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-02-16 01:59:59 +0000

    4-print_alphabt.c

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: printing all lowercase alphabet except for(q,e).
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+	{
+	char alph = 'a';
+
+	while (alph <= 'z')
+	{
+	if (alph == 'q' || alph == 'e')
+	{
+	alph++;
+	continue;
+	}
+	else
+	{
+	putchar(alph);
+	alph++;
+	continue;
+	}
+	}
+	puchar('\n');
+	return (0);
+}
