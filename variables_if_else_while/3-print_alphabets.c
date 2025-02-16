79b145b618e870dca34bff3ca908b17ecd17d632
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-02-16 00:59:50 +0000

    3-print_alphabets.c

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -8,25 +8,26 @@
 
 int main(void)
 {
-        char ALPH = 'A';
-        char alph = 'a';
+	char ALPH = 'A';
+	char alph = 'a';
 
-        while (alph <= 'z')
-        {
-                putchar(alph);
-                alph++;
-                if (alph > 'z')
-                {
-                        while(ALPH <= 'Z')
-                        {
-                                putchar(ALPH);
-                                        ALPH++;
-                        }
-                }
-                else{
-                        continue;
-                }
-        }
+	while (alph <= 'z')
+	{
+	putchar(alph);
+	alph++;
+	if (alph > 'z')
+	{
+	while (ALPH <= 'Z')
+	{
+	putchar(ALPH);
+	ALPH++;
+	}
+	}
+	else
+	{
+	continue;
+	}
+	}
 	putchar('\n');
 	return (0);
 }
