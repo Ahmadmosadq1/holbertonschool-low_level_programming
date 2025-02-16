94ffcfc42ce49c87fa6dffaaad3e8ea192b88a76
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-02-16 17:45:03 +0000

    7-print_tebahpla.c

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: This script prints digits from 0-9 using only putchar.
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char n = 'z';
+
+	while (n >= 'a')
+	{
+	n--;
+	putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
