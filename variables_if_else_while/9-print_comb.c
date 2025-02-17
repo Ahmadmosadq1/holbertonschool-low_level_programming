91e8bb1807b6704db8f871807e0b0b5b9ef1b0e5
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-02-17 01:37:41 +0000

    9-print_comb.c

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: printing number from 0-9 sperated by space and comma.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+	if (num == 9)
+	{
+	putchar(num + '0');
+	break;
+	}
+	else
+	{
+	putchar(num + '0');
+	putchar(',');
+	putchar(' ');
+	num++;
+	}
+	}
+	putchar('\n');
+	return (0);
+}
+	
