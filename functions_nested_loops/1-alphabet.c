24aa9bd42453694cb95e8f7b13deebdef4d04d1d
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-02-19 06:50:37 +0000

    1-apphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include "main.h"
+/*
+ * main - Entry point
+ *
+ * Description: creating a void function that print_alphapbet.
+function print_alphabet
+ 
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+	}
+}
+
