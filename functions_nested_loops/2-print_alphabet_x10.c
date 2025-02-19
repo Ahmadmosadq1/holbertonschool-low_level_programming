3d50df753763c7d1089e7be9424aa81f2695f529
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-02-19 17:33:17 +0000

    2-print_alphabet_x10.c

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * void print_alphabet_x10(void); - Entery point. 
+ *
+ * Description: t prints the all aplphabets 10 times follwed by new line
+ * Return: no need as it is a void function
+ */
+
+void print_alphabet_x10(void)
+{
+	char alpha;
+	int counter = 0;
+
+	while (counter < 10)
+	{	
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+	_putchar(alpha);
+	if (alpha == 'z')
+	{
+	_putchar('\n');
+	continue;
+	}
+	else
+	{
+	continue;
+	}
+	}
+	counter++;
+	}
+}
+
