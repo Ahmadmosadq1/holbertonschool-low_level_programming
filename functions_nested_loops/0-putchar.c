0917246cf7f3de0b86c5848994672a5f96e4ad25
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-02-19 05:56:58 +0000

    0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -3,16 +3,17 @@
 /**
  * main - Entry point
  *
- * Description: this script will call a userdefined function 
+ * Description: this script will call a userdefined function
  * ftom thr header file (main.h)
  * the called function is _putchar().
  * Return: Always 0 (Success)
  */
 
-int main (void)
+int main(void)
 {
 	char string[] = "_putchar";
 	int i;
+
 	for (i = 0; string[i] != '\0'; i++)
 	{
 	_putchar(string[i]);
