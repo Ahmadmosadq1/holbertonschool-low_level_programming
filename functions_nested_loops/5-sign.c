03ffaf9718bac951990cee5c539e3c2b760c331e
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-02-20 01:49:26 +0000

    5-sign.c

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -16,7 +16,7 @@ int print_sign(int n)
 	_putchar('+');
 	return (1);
 	}
-	else if(n == 0)
+	else if (n == 0)
 	{
 	_putchar(0 + '0');
 	return (0);
@@ -25,9 +25,7 @@ int print_sign(int n)
 	{
 	_putchar('-');
 	return (-1);
-
 	}
-	
 	return (0);
 }
 
