a93ac517018bbd73804e047b7439cdc0506d404f
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-02-20 17:13:18 +0000

    6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - Entry point
+ *
+ * Description: 'the program prints absulute value
+ * @int: describe the parameter
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _abs(int num)
+{
+	if (num < 0)
+	return (num * -1);
+	else
+	return (num);
+	return (0);
+}
+
