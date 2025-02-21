80a1c760d7972c204fd85daff440a1bd36a28567
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-02-21 08:06:47 +0000

    10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *add - Entry point
+ *
+ *Description: 'the program's description'
+ *@num1 : describe the parameter
+ *@num2 : describe the parameter
+ *Return: Always 0 (Success)
+ */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
