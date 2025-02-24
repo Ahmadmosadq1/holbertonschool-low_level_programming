1c1f5cf06b2a70cc70cad63baef69442339513a3
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-02-24 21:33:36 +0000

    9-fizz_buzz.c

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -15,7 +15,11 @@ int main(void)
 
 	for (count = 1; count <= 100; count++)
 	{
-		if (count % 3 == 0 && count % 5 == 0)
+		if (count == 0)
+		{
+			printf("%d", count);
+		}
+		else if (count % 3 == 0 && count % 5 == 0)
 		{
 			printf("FizzBuzz");
 			printf(" ");
@@ -29,10 +33,6 @@ int main(void)
 		{
 			printf("Fizz");
 			printf(" ");
-		}
-		else if ( count == 100)
-		{
-			printf("%d", count);
 		}
 
 	
