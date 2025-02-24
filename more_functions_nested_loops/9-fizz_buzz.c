f5b7bd5c381671d3e1c71827364a116dd8c14af2
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-02-24 00:23:04 +0000

    9-fizz_buzz.c

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description :printing numbers from (0-100) for fuzz buzz game
+ *.
+ *@void
+ *
+ * Return: void.
+ */
+int main(void)
+{
+	int count;
+
+	for (count = 1; count < 101; count++)
+	{
+		if (count % 3 == 0 && count % 5 == 0)
+		{
+			printf("FizzBuzz");
+			printf(" ");
+		}
+		else if (count % 3 == 0)
+		{
+			printf("Fizz");
+			printf(" ");
+
+		}
+		else if (count % 3 == 0 && count % 5 == 0)
+		{
+			printf("Buzz");
+			printf(" ");
+		}
+		else
+			printf("%d ", count);
+	}
+	return (0);
+}
+
+
