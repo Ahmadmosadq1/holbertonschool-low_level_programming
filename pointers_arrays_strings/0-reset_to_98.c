2f8ca239b6c4cbd9f869eed4f3eea6e15337b8fb
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-02-25 10:22:20 +0000

    0-reset_to_98.c

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+#ifndef "main.h"
+#define "main.h"
+
+/**
+ * reset_to_98 - Entry point
+ *
+ * Description :taking a poniter to int and updates it
+ * to 98
+ *
+ * @n : the value of the pointer
+ *.
+ * Return: void.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
