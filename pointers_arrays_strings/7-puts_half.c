e6e0950d2beb839942f6705a9495c01833f579cd
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-02-27 22:56:38 +0000

    7-puts_half

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -11,13 +11,18 @@
 void puts_half(char *str)
 {
 	int i =0;
-	int j;
+	int j ;
 	
 	/*calculating the length*/
 	while (str[i] != 0)
 	{
 		i++;
 	}
+	if (i % 2 == 1)
+		j = (i-1)/2;
+	else if ( i % 2 == 0)
+		j = i/2;
+
 
 	/*printing half the string*/
 
@@ -27,3 +32,4 @@ void puts_half(char *str)
 	}
 _putchar('\n');
 }
+
