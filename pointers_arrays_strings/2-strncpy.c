1418aa9d9ae895f0734889fb6b5f1554b585679e
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-02-28 05:04:11 +0000

    2-strncpy.c

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -4,28 +4,26 @@
  * *_strncpy - Entry point
  *
  * Description :copies a string from an array
+ *
  *@dest: destination array
  *@src : source array.
- 
+ *@n: number of appenong array elements
  * Return: dest.
  */
-
-
 char *_strncpy(char *dest, char *src, int n)
 {
-        int i = 0;
+	int i = 0;
 
-        while (src[i] != '\0')
-        {
-                dest[i] = src[i];
-		i++;
-        }
-
-	while (i < n)
+	while (src[i] != '\0' && i < n)
 	{
-	       	dest[i] = '\0';
+		dest[i] = src[i];
 		i++;
 	}
-        return (dest);
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
 }
 
