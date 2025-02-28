049f647e32ea0f477ccae25b9fcfa0143bfaf62f
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-02-28 18:07:48 +0000

    8-print_square.c

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -11,21 +11,20 @@
 
 void print_square(int size)
 {
-    int height, base;
+	int height, base;
 
-    if (size <= 0)  // Move the condition outside the loop
-    {
-        putchar('\n');
-        return;
-    }
-
-    for (height = 0; height < size; height++)
-    {
-        for (base = 0; base < size; base++)
-        {
-            putchar('#');
-        }
-        putchar('\n');
-    }
+	if (size <= 0) 
+	{
+	putchar('\n');
+	return;
+	}
+	for (height = 0; height < size; height++)
+	{
+	for (base = 0; base < size; base++)
+	{
+	putchar('#');
+	}
+	putchar('\n');
+	}
 }
 
