3cccc50075b6f74aa6f796b795e162732ee226b8
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-02-28 01:04:19 +0000

    8-print_array.c

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts_half - Entry point
+ *
+ * Description :prints half of a string.
+ *.
+ *@str : the parameters.
+
+ * Return: void.
+ */
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+	while ( i < n-1)
+	{
+		printf("%d, ", a[i]);
+		i++;
+		if (i == n-1)
+			printf("%d",a[i]);
+
+	}
+	_putchar('\n');
+}
