5800ec16f5aee46caf7c2535950c5664a6e22c0c
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-03-02 20:12:20 +0000

    0-memset.c

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * *_memset - Entry point
+ *
+ * Description :a function that fills specific char with n bytes
+ * pointed by s pointer
+ *.
+ *@s : the popinter.
+ *@b:the charachter to be printed
+ *@n : number of memory spaces to be filled with b.
+ * Return: void.
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	while (n--)
+	{
+		*s = (unsigned char)b;
+		s++;
+	}
+return (0);
+}
