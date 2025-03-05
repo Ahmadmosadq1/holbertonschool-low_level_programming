9833973a749093e911692496b80853fa7bf46f69
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-03-05 12:40:52 +0000

    8-print_diagsums.c

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - Entry point
+ *
+ * Description :prints the diagonals of a squarw array.
+ *@a : the array.
+ *@size : the size of the array.
+ * Return: void.
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int sum1;
+	int sum2;
+
+	for (i = 0; i < size; i++)
+	{
+		sum1 = sum1 + *(a + i * size + i);/*prints the first diagonal*/
+		sum2 = sum2 + *(a + i * size + (size - 1 - i));/*second diagonal*/
+	}
+	printf("%d %d \n", sum1, sum2);
+}
