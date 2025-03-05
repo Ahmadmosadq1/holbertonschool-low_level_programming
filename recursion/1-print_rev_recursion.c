655343972684ab8ed7a6a6f80057d8110167ace3
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-03-05 18:18:59 +0000

    1-print_rev_recursion.c

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+
+/**
+ * _print_rev_recursion - Entry point
+ *
+ * Description: This function prints the string in reverse.
+ *
+ * @s : the pointer.
+ *
+ * Return: Always 0 (Success)
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
