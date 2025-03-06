e92d1029e25aee569503d963846a0e094de93130
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-03-06 16:27:25 +0000

    4-pow_recursion.c

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - Entry point
+ *
+ * Description: This function takes the power y of x..
+ *
+ * @x :  the base number.
+ *
+ * @y : the power number.
+ *
+ * Return: return the result of the function
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 1)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (n * _pow_recursion(x, y));
+}
