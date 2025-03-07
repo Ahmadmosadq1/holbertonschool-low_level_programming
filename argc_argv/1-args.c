47c60c1f23c1facae9139a55060b551e8abb0aee
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-03-07 16:41:17 +0000

    1-args.c

---
+++ argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include  <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Description :main function prints its arguman count..
+ *.
+ *@argc : the number of the arrgument.
+ *
+ * @argv : argument vector.
+ *
+ * Return: 0 (success).
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}
+
