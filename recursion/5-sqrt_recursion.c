4c05c1227386e2a4ed86e801bfe41606bf13cf7f
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-03-08 15:08:36 +0000

    5-sqrt_recursion.c

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -18,5 +18,15 @@ int _sqrt_recursion(int n)
 	if (n < 0)
 		return (-1);
 
-	return (_sqrt_helper(y, n));
+	return (_sqrt_helper(n, y));
+}
+
+int _sqrt_helper(int n, int y)
+{
+
+	if (y * y > n)
+		return (-1);
+	if (y * y == n )
+		return (y);
+	return (_sqrt_helper(n, y + 1));
 }
