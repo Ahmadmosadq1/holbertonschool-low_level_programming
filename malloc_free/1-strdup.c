5badfacfb245c1a0d2297c7a5f0536ccad4602c2
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-03-11 16:16:28 +0000

    1-strdup.c

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - Entry point
+ *
+ * Description: copies a string to a heap memeory.
+ * @str : the input string.
+ *
+ * Return: arr.
+ */
+
+char *_strdup(char *str)
+{
+	int i = 0;
+	int j = 0;
+	char *arr;
+
+	/* finding the string length*/
+
+	while (str[i])
+	{
+		j++;
+	/* returing the pointer to space AKA heap*/
+
+	arr = malloc((j + 1) * sizeof(char));
+	if (str == NULL)
+		return (NULL);
+	if (arr == NULL)
+		return (NULL);
+	for (i = 0; i < j; i++)
+		arr[i] = str[i];
+	arr[i] = '\0';
+}
+return (arr);
+}
+
+
--- malloc_free/README.md
+++ malloc_free/README.md
@@ -3,3 +3,7 @@
 - Creating an array with dynamic memory allocation using malloc function.
 - this array is saved in heap memory during the runtime and will stay there as long as the program live.
 - you also need to handle two cases. if (size == 0), return NULL and if the malloc() fails to allocate memorty(arr == NULL), return NULL.
+
+1-strdup.c:
+- we have input (str) saved in stack and we want to save it in heap memory using malloc.
+- so it copies the str from stack into heap memeory ..aka relocation
