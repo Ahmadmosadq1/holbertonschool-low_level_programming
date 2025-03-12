fc33c0a75aa09c93a7cdae6e661c948ebe262c6e
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-03-12 20:50:55 +0000

    0-malloc_checked.c

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -3,16 +3,18 @@
 #include <stdlib.h>
 #include <limits.h>
 /**
- * main - Entry point
+ * malloc_checked - Entry point
  *
- * Description: This function prints a string using puts.
+ * Description: This function retunrs the pointer to integer
+ *
+ * @b : the byte size.
  * Return: Always 0 (Success)
  */
 
 void *malloc_checked(unsigned int b)
 {
 	int *arr;
-	
+
 	arr = malloc(b);
 	if (arr == NULL)
 		exit(98);
