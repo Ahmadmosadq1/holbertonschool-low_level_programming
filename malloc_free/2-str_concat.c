25ae890524eea2904eb23a4b9c7c32ea735e0107
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-03-12 14:07:32 +0000

    2-str_concat.c

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -20,9 +20,9 @@ char *str_concat(char *s1, char *s2)
 	char *arr;
 
 	if (s1 == NULL)
-		s1 = '\0';
+		*s1 = '\0';
 	if (s2 == NULL)
-		s2 = '\0';
+		*s2 = '\0';
 	while (s1[i] != '\0')
 		i++;
 	while (s2[j] != '\0')
