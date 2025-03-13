40b85366608c89bb76d4314a1b9c49c46f602ad6
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-03-13 19:24:15 +0000

    2-calloc.c

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -14,11 +14,10 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	unsigned int i;
 	char *arr;
-
+	if (nmemb == 0 || size == 0)
+		return (NULL);
 	arr = malloc(nmemb * size);
 	if (arr == NULL)
-		return (NULL);
-	if (nmemb == 0 || size == 0)
 		return (NULL);
 	for (i = 0; i < (nmemb * size); i++)
 		arr[i] = 0;
