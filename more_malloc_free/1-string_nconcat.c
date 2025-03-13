acfb0d4e6d76f6fddbc8694c410fc32a808590a7
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-03-13 15:43:59 +0000

    1-string_nconcat.c

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -14,16 +14,17 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int count1;
 	unsigned int count2;
 	char *arr;
+	
+	if (s1 == NULL)
+                s1 = "";
+        if (s2 == NULL)
+                s2 = "";
 
 	while (s1[i])
 		i++;
 	while (s2[j])
 		j++;
 
-	if (s1 == NULL)
-                s1 = "";
-        if (s2 == NULL)
-                s2 = "";
 	/*allocating heap memory for the both sizes + '\0'*/
 	arr = malloc((i + n + 1) * sizeof(char));
 	
