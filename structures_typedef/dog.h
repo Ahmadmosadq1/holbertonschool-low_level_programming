d624ff56827893d2502d7c5db5fb105495127671
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-03-14 08:53:52 +0000

    dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef dog_h
+#define dog_h
+/**
+ * struct dog - data structure for a dog
+ * @name: dog's name
+ * @age: dog's age.
+ * @owner: owner's name.
+ *
+ * Description: it is a struture contains dog's name, age and owner's name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
