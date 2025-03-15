d6b012a468ddad6e0232ec78db88d7a0fa2f7cb6
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-03-15 16:23:47 +0000

    4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -46,7 +46,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog->owner[_strlen(name)] ='\0';
 
 	/*assigning age*/
-	new_dog->age = age;
+	new_dog->age = ((int)(age * 10)) / 10.0;
 	return (new_dog);
 }
 
