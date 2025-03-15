651b7f07d08bc0b8a849affb893b8c916fd026e4
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-03-15 15:41:14 +0000

    4-new_dog.c

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,83 @@
+#include "dog.h"
+#include <stddef.h>
+#include <stdlib.h>
+char *_strncpy(char *dest, char *src, int n);
+int _strlen(char *s);
+/**
+ * new_dog- Entry point
+ *
+ * Description: printing the memeber of the data struct.
+ *
+ * @name: member#1.
+ * @age: member#2.
+ * @owner: member#3.
+ *
+ * Return: new_dog
+ */
+
+
+
+/*struct function*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	/*allocating memory size for the pointer*/
+	dog_t *new_dog = malloc(sizeof(dog_t));
+
+	if (new_dog == NULL)
+		return (NULL);
+	/*alloctaion memory size for name and make a copy*/
+	new_dog->name = malloc(_strlen(name) + 1);
+	_strncpy(new_dog->name, name, _strlen(name));
+
+	/*allocating memroy sixe for owner and make a copy*/
+	new_dog->owner = malloc(_strlen(name) + 1);
+	_strncpy(new_dog->owner, owner, _strlen(owner));
+
+	/*assigning age*/
+	new_dog->age = age;
+	return (new_dog);
+}
+
+/**
+ * _strlen - Calculates the length of a string.
+ * Description: Iterates through the string to find its length.
+ *@s: string length
+ * Return: The length of the string.
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*s++)
+	{
+		i++;
+	}
+	return (i);
+}
+/**
+ * _strncpy - Copies a string from source to destination.
+ * Description: Copies up to 'n' characters from src to dest.
+ * @dest :The destination string.
+ * @src: The source string.
+ * @n: The number of characters to copy.
+ *
+ * Return: A pointer to the destination string.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
+
