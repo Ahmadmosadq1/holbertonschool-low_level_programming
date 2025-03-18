baf99339be3ae2f7726b188f9e57c2650fd54e05
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-03-18 19:57:18 +0000

    2-int_index.c

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - Entry point
+ *
+ * Description: funtion pointers for index searching.
+ * @array: the array.
+ * @size: the number of elemnts
+ * @cmp: the funtion pointer
+ * Return: return -1 (failed)
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+	for(i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return(i);
+	}
+	return (-1);
+}
+
