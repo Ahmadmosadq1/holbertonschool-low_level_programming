a6f18a8de81485238ec4819a7a3d959502d5716e
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-03-20 19:30:15 +0000

    1-print_numbers.c

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -20,7 +20,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n - 1; i++)
 	{
 		if (separator)
-			printf(", ");
+			printf("%s", separator);
 		printf("%d", va_arg(arg, int));
 	}
 	printf("\n");
