31405fc0f47b3c757e6e1b9c2d743516cabbfdf2
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-03-21 15:37:42 +0000

    3-print_all.c

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -8,19 +8,46 @@
  * Return: Always 0 (Success)
  */
 
+
+void print_char(va_list arg)
+	{
+		printf("%c", va_arg(arg, int));
+	}
+void print_int(va_list arg)
+	{
+		printf("%d", var_arg(arg, int));
+	}
+void print_float(va_list arg)
+	{
+		printf("%f", va_arg(arg, double));
+	}
+void print_string(va_list arg)
+{
+	char *str = va_arg(arg, char *);
+
+	if (str == NULL)
+		printf("%s", "(nil)");
+}
+void func_selector(va_list)
+{
+	print_char;
+	print_int;
+	print_float;
+	print_string;
+}
+
+
 void print_all(const char * const format, ...)
 {
-	int i;
 	va_list arg;
-	int *str;
-
-	va_start(arg, format);
-	
-	while (va_arg(arg, fromat) != NULL)
+	va_start (arg, fromat);
+	char *selector[4] = {'c', 'i', 'f'. 's'};
+	int i;
+	int j
+	while (format && fromat[i])
 	{
-		str = va_arg(arg, format);
-		if (isdigit(str))
-			
-		if(!isdigit(str))
-			continue;
+		if (format[i] == separator[i])
+		{
+			func_selector[i](arg)
+
 
