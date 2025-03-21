43e0a19c885c3e42f43fd562bea724134bb879d3
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-03-21 08:31:33 +0000

    2-print_strings.c

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -23,7 +23,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (str == NULL)
 			printf("%s", "(nil)");
 		else
-			rintf("%s", str);
+			printf("%s", str);
 		if (i < n - 1 && separator)
 			printf("%s", separator);
 	}
