46785c0abd9ae0b9c9d7863bd9a0789d9a8e128d
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-03-22 19:53:50 +0000

    commiting

---
+++ function_pointers/3-main.c
@@ -0,0 +1,18 @@
+#include "3-calc.h"
+#include <stdio>
+
+/**
+ * puts_half - Entry point
+ *
+ * Description :prints half of a string.
+ *.
+ *@str : the parameters.
+ 
+ * Return: void.
+ */
+
+int main()
+{
+
+
+
