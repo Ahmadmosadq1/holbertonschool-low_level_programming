e6d61359c00f8362013ac131a7debb0c88152290
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-03-22 20:36:28 +0000

    commit

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,5 +1,6 @@
 #include "3-calc.h"
-#include <stdio>
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * puts_half - Entry point
@@ -11,8 +12,36 @@
  * Return: void.
  */
 
-int main()
+int main(int argc, char *argv[])
 {
+    int a, b;
+    int (*operation)(int, int);
 
+    if (argc != 4)
+    {
+        printf("Error\n");
+        exit(98);
+    }
+
+    operation = get_op_func(argv[2]);
+
+    if (!operation)
+    {
+        printf("Error\n");
+        exit(99);
+    }
+
+    a = atoi(argv[1]);
+    b = atoi(argv[3]);
+
+    if ((argv[2][0] == '/' || argv[2][0] == '%') && b == 0)
+    {
+        printf("Error\n");
+        exit(100);
+    }
+
+    printf("%d\n", operation(a, b));
+    return (0);
+}
 
 
--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,3 +1,5 @@
+#include "3-calc.h"
+
 int op_add(int a, int b)
 {
 	return (a + b);
@@ -12,7 +14,7 @@ int op_sub(int a, int b)
 
 int op_mul(int a, int b)
 {
-	return (a * B);
+	return (a * b);
 }
 
 int op_div(int a, int b)
