44a7ccaa8b7a1734499781d7aca90a0c48cd1570
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-04-04 17:50:25 +0000

    0-read_textfile.c

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -5,10 +5,12 @@
 #include <fcntl.h>
 
 /**
- * main - Entry point
+ * main - read_textfile
  *
- * Description: This function prints a string using puts.
- * Return: Always 0 (Success)
+ * Description: This function writes a text from a file to a buffer
+ * @filename: the textfile to copy from.
+ * @letters: the number of chars in the textfile
+ * Return: w_fd
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
@@ -17,19 +19,19 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int w_fd;
 	int r_fd;
 	char *buffer = malloc(letters * sizeof(char));
-	
+
 	if (filename == NULL)
 		return (0);
 	if (buffer == NULL)
-                return (0);
- 	fd = open(filename, O_RDONLY);
+		return (0);
+	fd = open(filename, O_RDONLY);
 	r_fd = read(fd, buffer, letters);
 	if (r_fd == -1)
 		return (0);
 	w_fd = write(STDOUT_FILENO, buffer, r_fd);
 		if (w_fd == -1)
 			return (0);
-	if(w_fd != r_fd)
+	if (w_fd != r_fd)
 	{
 		free(buffer);
 		close(fd);
--- file_io/README.md
+++ file_io/README.md
@@ -1,1 +1,12 @@
-READ
+0-read_textfile:
+
+- We want to copy the contents of the file from a text to a buffer.
+- We first make sure filename does not point to NULL.
+- We create a char data type buffer with malloc that should accommodate whatever is in the filename and also check it doesn't point to NULL.
+- Size of memory should be multiplied by letters to accommodate it.
+- We create our file descriptor by opening the filename with (open sys call).
+- Then we read the file in a buffer and assign it to r_fd and then we write the r_fd to standard output.
+- Naturally, if either of read or write failed, they will return -1.. so if that's the case, we return 0.
+- Then we compare w_fd and r_fd to confirm that what we wrote is in fact what we read. Otherwise, return 0.
+- Finally, we free the buffer and close our fd file.
+
