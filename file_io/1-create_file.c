f687a858bbae1f4237cc60c70698f5ea039a2c67
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-04-05 13:48:55 +0000

    1-create_file.c

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -16,34 +16,24 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd;
 	int w_fd;
-	char buffer[2024];
-	char reader;
 	int i = 0;
-	int j = 0;
 	
 	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-	reader = read(STDIN_FILENO, buffer, 2024);
-	if (reader == -1)
-		return (-1);
-	while (i < reader)
-		j++;
-	text_content = malloc(sizeof(char) * (i + 1));
-	if (text_content == NULL)
-		return (-1);
-	for (; i < j; i++)
-		text_content[i] = buffer[i];
-	text_content[i] = '\0';
-	w_fd = write(fd, text_content, j);
+	if (text_content != NULL)
+	{
+		while (text_content[i])
+			i++;
+	}
+	w_fd = write(fd, text_content, i);
 	if (w_fd == -1)
+	{
+		close(fd);
 		return (-1);
-	free(text_content);
+	}
 	close(fd);
 	return (1);
 }
-
-
-
