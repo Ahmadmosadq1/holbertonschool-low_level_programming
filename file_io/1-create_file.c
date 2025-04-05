26d9659f5f8c8e620c821a386c35be57fe7b5664
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-04-05 13:54:35 +0000

    1-create_file.c

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -5,11 +5,12 @@
 #include <fcntl.h>
 
 /**
- * print_numbers - Entry point
+ * create_file - Entry point
  *
- * Description :prints number from 0 to 9 using _putchar
- *.
- * Return: void.
+ * Description :create a file with text filled by user
+ *@filename: the file to be created.
+ *@text_content: the text
+ * Return: 1 (success).
  */
 
 int create_file(const char *filename, char *text_content)
@@ -17,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 	int fd;
 	int w_fd;
 	int i = 0;
-	
+
 	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
