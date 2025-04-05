70511be613ea7cd259469e94a38180b4491263bd
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-04-05 14:44:28 +0000

    2-append_text_to_file.c

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,39 @@
+#include <unistd.h>
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+#include <fcntl.h>
+
+/**
+ * create_file - Entry point
+ *
+ * Description :create a file with text filled by user
+ *@filename: the file to be created.
+ *@text_content: the text
+ * Return: 1 (success).
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int i;
+	int fd;
+	int w_fd;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if(fd == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		while (text_content[i])
+			i++;
+	}
+	w_fd = write(fd, text_content, i);
+	if (w_fd == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+	return (1);
+}
