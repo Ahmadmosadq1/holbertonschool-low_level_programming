dce4c78814172f8cac4f19e9b46badb36bec9085
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-04-05 14:50:25 +0000

    2-append_text_to_file.c

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -5,7 +5,7 @@
 #include <fcntl.h>
 
 /**
- * create_file - Entry point
+ * append_text_to_file - Entry point
  *
  * Description :create a file with text filled by user
  *@filename: the file to be created.
@@ -21,7 +21,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_APPEND);
-	if(fd == -1)
+	if (fd == -1)
 		return (-1);
 	if (text_content != NULL)
 	{
