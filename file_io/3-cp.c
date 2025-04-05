39aa8ac4a00fa8c361b932ff022d96710a379f6d
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-04-05 17:59:59 +0000

    3-cp.c

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -2,11 +2,12 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
  * main - copies content of one file to another
  * @ac: argument count
  * @av: argument vector
- * Return: 0 on success, exit with code on error
+ * Return: 0 on success, exits on error
  */
 int main(int ac, char **av)
 {
@@ -14,33 +15,45 @@ int main(int ac, char **av)
 	char buf[1024];
 
 	if (ac != 3)
-		dprintf(2, "Usage: %s file_from file_to\n", av[0]), exit(97);
+	{
+		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", av[0]);
+		exit(97);
+	}
 
 	f_from = open(av[1], O_RDONLY);
 	if (f_from == -1)
-		dprintf(2, "Error: Can't read from file %s\n", av[1]), exit(98);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
 
 	f_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (f_to == -1)
-		dprintf(2, "Error: Can't write to %s\n", av[2]), close(f_from), exit(99);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		close(f_from);
+		exit(99);
+	}
 
 	while ((r = read(f_from, buf, 1024)) > 0)
 	{
 		w = write(f_to, buf, r);
 		if (w != r)
-			dprintf(2, "Error: Can't write to %s\n", av[2]),
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 			close(f_from), close(f_to), exit(99);
+		}
 	}
-
 	if (r == -1)
-		dprintf(2, "Error: Can't read from file %s\n", av[1]),
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		close(f_from), close(f_to), exit(98);
-
+	}
 	if (close(f_from) == -1)
-		dprintf(2, "Error: Can't close fd %d\n", f_from), exit(100);
-
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f_from), exit(100);
 	if (close(f_to) == -1)
-		dprintf(2, "Error: Can't close fd %d\n", f_to), exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f_to), exit(100);
 
 	return (0);
 }
+
